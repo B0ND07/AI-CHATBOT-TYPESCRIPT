1977ff391ecf849139298ab47dd6355e85d05c47
B0ND07 <mhashim@gmail.com>
2024-03-09 00:05:59 +0530

    deploy: update origin & api url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ config();
 const app = express();
 
 
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"https://botopenai.netlify.app",credentials:true}))
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan("dev"));
